220563ec8d626ae56ce78adadccffd148db62293
Akshit-454 <jtzv9442@leeds.ac.uk>
2025-09-30 18:55:05 +0000

    Complelted till 3.2.2

---
+++ tasks/task2_5_1/try.kt
@@ -0,0 +1,6 @@
+fun main() {
+    var num = 22
+    println(num)
+    num = 50 
+    println(num)
+}
--- tasks/task3_1/Args.kt
+++ tasks/task3_1/Args.kt
@@ -1,1 +1,14 @@
 // Task 3.1: command line arguments
+import kotlin.system.exitProcess
+fun main ( args:Array<String>){
+    
+    if (args.size != 2){
+        println("Error: Please provide exactly two arguments.")
+        exitProcess(1)
+    }
+    else{
+        println(args[0])
+        println(args[1])
+    }
+
+}
