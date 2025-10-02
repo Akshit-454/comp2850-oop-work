bb333801d1cd470a3066e41bac22b620c4f25d40
Akshit-454 <jtzv9442@leeds.ac.uk>
2025-10-02 20:16:32 +0000

    completdt till 4.2

--- tasks/task4_3/ModuleGrade.kt
+++ tasks/task4_3/ModuleGrade.kt
@@ -3,20 +3,26 @@ import kotlin.math.roundToInt
 
 fun main(args: Array<String>) {
     if (args.size != 3){
-        println("ERROR: Please give 3 module marks")
+        println("ERROR: Please give 3 module marks only")
         exitProcess(1)
     }
     val mark1 = args[0].toInt()
     val mark2 = args[1].toInt()
     val mark3 = args[2].toInt()
-    val average = round((mark1 + mark2 + mark3)/3.0).toInt() //use .toInt() only if u dont wqant final answer in decimal
 
-    val grade = when (average){
+    if (!(mark1 in 0..100 && mark2 in 0..100 && mark3 in 0..100)){
+        println("ERROR: Please give 'PROPER' module marks")
+        exitProcess(1)
+    }
+    val average = ((mark1 + mark2 + mark3)/3.0) //use .toInt() only if u dont wqant final answer in decimal
+    val rounded_average = average.roundToInt()
+    val grade = when (rounded_average){
         in 0..39 -> "Fail"
         in 40..69 -> "Pass"
         in 70..100 -> "Distinction"
+        else -> "Information not given"
     }
-    println("Average mark = $average")
+    println("Rounded Average mark = $rounded_average")
     println("Grade = $grade")
     
 }
