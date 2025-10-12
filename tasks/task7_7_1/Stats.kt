72ed466904cd700e492740cd373e5f27d2de7774
Akshit-454 <jtzv9442@leeds.ac.uk>
2025-10-12 20:09:59 +0000

    completed data collections

--- tasks/task7_7_1/Stats.kt
+++ tasks/task7_7_1/Stats.kt
@@ -1,1 +1,45 @@
 // Task 7.7.1: stats for a numeric dataset
+
+fun readData()= buildList{
+    println("Enter floating data-values and Press q to exit:  ")
+   
+    while (true){
+        val input = readln()
+        if (input == "q" || input == "Q"){
+            break
+        }
+        else{
+            val num = input.toFloat()
+            add(num)
+        }
+    }
+}
+
+fun median(list: List<Float>):Float{
+    if (list.isEmpty()){
+        println("List is empty")
+    }
+    val size_list = list.size
+    val sorted = list.sorted()
+    var median_ans = 0.0f
+    if (size_list%2==0){
+         median_ans = (sorted[(size_list/2)]+sorted[(size_list/2)-1])/2
+    }
+    else {
+         median_ans = sorted[(size_list)/2]
+    }
+    return median_ans
+    //println("Median of floating point values = $median_ans ")
+
+}
+fun display(list:List<Float>){
+    val median = median(list)
+    val average = list.average()
+    val min = list.min()
+    val max = list.max()
+    println("Stats: \nMedian = $median \nMean = $average \nMin = $min \nMax = $max \n")
+}
+fun main(){
+    val List  = readData()
+    display(List)
+}
--- tasks/task7_7_2/Contacts.kt
+++ tasks/task7_7_2/Contacts.kt
@@ -1,1 +1,29 @@
 // Task 7.7.2: contact database, using a map
+val dataMap = mutableMapOf("Akshit" to "+91 8056117918" , "Saakshi" to "+91 638219937")
+
+fun Database(name: String){
+    if (name in dataMap.keys){
+
+        println("$name : ${dataMap[name]}")
+    }
+    else {
+        println("Person Not Found")
+        println("Enter person's number to store: ")
+        val number = readln()
+        dataMap[name] = number
+    }
+}
+fun main(){
+    println("Connecting database... ")
+    while(true){
+        println("Please enter Person name and enter z to exit : ")
+        val input = readln()
+        if (input=="z"||input=="Z"){
+            break
+        }
+        else {
+            Database(input)
+        }
+    }
+}
+
