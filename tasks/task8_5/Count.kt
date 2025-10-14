a3558f0b3f571bd3681f69b0ca7d34dfe43d1a8c
Akshit-454 <jtzv9442@leeds.ac.uk>
2025-10-14 13:58:59 +0000

    completed till 8

---
+++ tasks/task8_5/Count.kt
@@ -0,0 +1,16 @@
+
+fun String.howMany(include:(Char)-> Boolean):Int{
+    var count = 0
+    for (char in this){
+        if(include(char)){
+            count+=1
+        }
+    }
+    return count
+}
+
+fun main(){
+    val string = "akshit"
+    val num = string.howMany{it in string}
+    println(num)
+}
